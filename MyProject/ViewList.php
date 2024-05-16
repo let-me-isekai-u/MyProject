53d301d6b70bf9261b3bd61fc6dea47ec04fdbd4
let-me-isekai-u <141901221+let-me-isekai-u@users.noreply.github.com>
2024-05-16 17:45:02 +0700

    Update ViewList.php

--- MyProject/ViewList.php
+++ MyProject/ViewList.php
@@ -10,13 +10,16 @@
 
     <form action="./delete.php" method = "post">
         <input type="text" name = "id">
-        <button type = "submit"> Xóa cmn đi</button>
+        <button type = "submit"> Xóa đi</button>
     </form>
 
+        <form action="./logout.php" method = "post">
+            <button type = "submit">log out</button>
+        </form>
     <?php
     try{
         $conn = mysqli_connect("localhost", "root","123456a@","MyDatabase");
-        $rows = mysqli_query($conn, "select id, name, email from student order by id limit 20");
+        $rows = mysqli_query($conn, "select id, name, email, image from student order by id limit 20");
         if(mysqli_num_rows($rows)==0){
             $row=[];
         }
@@ -28,8 +31,16 @@
         $id = $student['id'];
         $name = $student['name'];
         $email = $student['email'];
+        $image = $student['image'];
+    
+
+        // $image_name = $_FILES['image']['name'];
+        // $image_tmp = $_FILES['image']['tmp_name'];
+      
+
+
         echo $id . ',' . $name . ',' . $email . '<br>';
-        
+        echo "<br> <img src = '".$image."' width = '300px'>";
     }
     ?>
 </html>
