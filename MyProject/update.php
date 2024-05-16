778984803a31735ce90e2ed50a4095b151b5d2e2
let-me-isekai-u <141901221+let-me-isekai-u@users.noreply.github.com>
2024-05-16 17:46:02 +0700

    Update update.php

--- MyProject/update.php
+++ MyProject/update.php
@@ -12,10 +12,41 @@
 
     </form>
     <body>
+
+
+<?php
+    session_start(); 
+
+
+    if (!isset($_SESSION['username'])) {
+        header("Location: login.php"); 
+        exit();
+    }
+    
+?>
+
         <?php
-        $conn = mysqli_connect("localhost","root","123456a@","MyDatabase");
-        if($conn->connect_error) 
-        die("không kết nối được". $conn->connect_error);
+
+try{
+    $conn = mysqli_connect("localhost", "root","123456a@","MyDatabase");
+    if($conn->connect_error) 
+    die("không kết nối được". $conn->connect_error);
+  
+    $rows = mysqli_query($conn, "select id, name, email from student order by id limit 20");
+    if(mysqli_num_rows($rows)==0){
+        $row=[];
+    }
+}catch(Exception $e){
+    echo"Error". $e->getMessage();
+}if(!empty($rows));
+
+        // $conn = mysqli_connect("localhost","root","123456a@","MyDatabase");
+        // if($conn->connect_error) 
+        // die("không kết nối được". $conn->connect_error);
+      
+       
+    
+
         if(isset($_POST['submit'])){
             $id = $_POST['id'];
             $name = $_POST['name'];
@@ -24,6 +55,7 @@
             $image_name = $_FILES['image']['name'];
             $image_tmp = $_FILES['image']['tmp_name'];
             $target_dir = "./". $image_name;
+            
 
             if (move_uploaded_file($image_tmp, $target_dir)) {
                 $query = "update student set name = '$name', email = '$email', image = '$target_dir' WHERE id = $id";
@@ -37,7 +69,15 @@
                 }
             } else {
                 echo "Lỗi khi tải lên file ảnh.";
-            }
+            }        
+        }
+
+        foreach($rows as $student){
+            $id = $student['id'];
+            $name = $student['name'];
+            $email = $student['email'];
+            echo $id . ',' . $name . ',' . $email . '<br>';
+            
         }
         
         mysqli_close($conn);
