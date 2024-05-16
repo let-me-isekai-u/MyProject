1b5697152af8385c7db04e7b3dae427b9059e9d9
let-me-isekai-u <141901221+let-me-isekai-u@users.noreply.github.com>
2024-05-16 17:44:44 +0700

    Update login.php

--- MyProject/login.php
+++ MyProject/login.php
@@ -1,9 +1,9 @@
 <?php
-session_start(); // Bắt đầu session
+session_start(); 
 
-// Kiểm tra xem người dùng đã đăng nhập hay chưa
+
 if (isset($_SESSION['username'])) {
-    header("Location: welcome.php");
+  
     exit();
 }
 
@@ -11,29 +11,29 @@ if (isset($_POST['submit'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Kết nối đến cơ sở dữ liệu
+   
     $conn = mysqli_connect("localhost", "root", "123456a@", "MyDatabase");
 
-    // Kiểm tra kết nối
+  
     if (!$conn) {
         die("Kết nối thất bại: " . mysqli_connect_error());
     }
 
-    // Truy vấn cơ sở dữ liệu để lấy thông tin người dùng
+  
     $query = "SELECT * FROM student WHERE username='$username' AND password='$password'";
     $result = mysqli_query($conn, $query);
 
     if (mysqli_num_rows($result) > 0) {
-        // Lưu thông tin người dùng vào session
+       
         $_SESSION['username'] = $username;
         echo "Đăng nhập thành công!";
-        header("Location: ViewList.php"); // Chuyển hướng đến trang welcome.php
+        header("Location: ViewList.php"); 
         exit();
     } else {
         echo "Sai tên đăng nhập hoặc mật khẩu!";
     }
 
-    // Đóng kết nối
+
     mysqli_close($conn);
 }
 ?>
