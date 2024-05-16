b012339c37bcf1ee43c4476a86540020341fc33f
let-me-isekai-u <141901221+let-me-isekai-u@users.noreply.github.com>
2024-05-16 17:40:38 +0700

    Update MyDatabase.sql

--- MyProject/MyDatabase.sql
+++ MyProject/MyDatabase.sql
@@ -1,18 +1,55 @@
-create database MyDatabase;
-create table if not exists student(
-	id int(10) not null unique auto_increment,
-	name varchar(100),
-    email varchar(55),
-	image varchar(255),
-    username varchar(50),
-    password varchar(50)
-);
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mydatabase
+-- ------------------------------------------------------
+-- Server version	8.0.34
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-select * from student;
-insert into student(name, email)
-values('ABC', 'ABC@gmail.com'),
-	  ('XYZ', 'XYZ@gmail.com'),
-      ('QKA', 'QKA@gmail.com')
-	
-	  
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(55) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(10) DEFAULT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES (1,'ABC','ABC@gmail.com',NULL,NULL,NULL),(2,'XYZ','XYZ@gmail.com',NULL,NULL,NULL),(3,'QKA','QKA@gmail.com',NULL,NULL,NULL),(4,NULL,NULL,NULL,'quy','123123'),(5,NULL,NULL,NULL,'quy1','123123'),(6,NULL,NULL,NULL,'dao1','123'),(7,'dao1','dao1@gmail.com','./hinh-nen-ngo-nghinh-anh-meo-cute-nupet-new-5.jpg',NULL,NULL),(8,NULL,NULL,NULL,'dao2','123'),(9,'dao2','dao2@gmail.com','./hinh-nen-ngo-nghinh-anh-meo-cute-nupet-new-5.jpg',NULL,NULL);
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-16 17:38:25
